07c0a716802aca8c630f0b813efa9b4426469bd3
Stefan Marks <stefanmarks@users.noreply.github.com>
2025-05-07 16:04:06 +1200

    OSC IO Device script more flexible

--- Assets/Scripts/OSC_IODevice.cs
+++ Assets/Scripts/OSC_IODevice.cs
@@ -5,25 +5,28 @@ using UnityEngine.Events;
 
 public class OSC_IODevice : MonoBehaviour, IOSCVariableContainer
 {
+	[System.Serializable]
+	public struct InputToEventMap
+	{
+		public string           OSC_Name;
+		public UnityEvent<bool> OnInputActive;
+	}
+
 	public string Prefix = "/tracked_device";
 
-	public UnityEvent<bool> OnInput1Event;
-	public UnityEvent<bool> OnInput2Event;
-	public UnityEvent<bool> OnInput3Event;
-	public UnityEvent<bool> OnInput4Event;
+	public List<InputToEventMap> Inputs;
 
 
 	public void Start()
 	{
-		m_io1 = new OSC_BoolVariable(Prefix + "/input1");
-		m_io2 = new OSC_BoolVariable(Prefix + "/input2");
-		m_io3 = new OSC_BoolVariable(Prefix + "/input3");
-		m_io4 = new OSC_BoolVariable(Prefix + "/input4");
+		m_inputs = new List<OSC_BoolVariable>();
 
-		m_io1.OnDataReceived += var => { OnUpdate(var, OnInput1Event); };
-		m_io2.OnDataReceived += var => { OnUpdate(var, OnInput2Event); };
-		m_io3.OnDataReceived += var => { OnUpdate(var, OnInput3Event); };
-		m_io4.OnDataReceived += var => { OnUpdate(var, OnInput4Event); };
+		foreach (var i in Inputs)
+		{
+			OSC_BoolVariable oscVar = new OSC_BoolVariable(Prefix + "/" + i.OSC_Name);
+			oscVar.OnDataReceived += var => { OnUpdate(var, i.OnInputActive); };
+			m_inputs.Add(oscVar);
+		}
 	}
 
 
@@ -41,9 +44,9 @@ public class OSC_IODevice : MonoBehaviour, IOSCVariableContainer
 
 	public List<OSC_Variable> GetOSC_Variables()
 	{
-		return new List<OSC_Variable>() { m_io1, m_io2, m_io3, m_io4 };
+		return new List<OSC_Variable>(m_inputs);
 	}
 
 
-	protected OSC_BoolVariable m_io1, m_io2, m_io3, m_io4;
+	protected List<OSC_BoolVariable> m_inputs;
 }
