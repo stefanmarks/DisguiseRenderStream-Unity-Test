f2d854695b9bb9fa2525bf1f59ffd63496880252
Stefan Marks <stefanmarks@users.noreply.github.com>
2025-05-21 13:20:48 +1200

    Added debug output

--- Assets/DisguiseUnityRenderStream/DisguiseCameraCapture.cs
+++ Assets/DisguiseUnityRenderStream/DisguiseCameraCapture.cs
@@ -337,6 +337,8 @@ class DisguiseRenderStream
 //             captureAfterPostProcess.height.value = (Int32)stream.height;
 // #endif
             camera.enabled = true;
+
+            Debug.Log($"Created Render Stream '{camera.name}' with capture.ApplyCameraData={capture.ApplyCameraData}");
         }
 
         // stop template cameras impacting performance
@@ -462,6 +464,11 @@ class DisguiseRenderStream
                                         m.rotation
                                     );
                                     transform.localScale = m.lossyScale;
+
+                                    if (dcc != null)
+                                    {
+                                        dcc._dbgOutsideTransformChange = true;
+                                    }
                                 }
                                 i += 16;
                             }
@@ -591,6 +598,9 @@ public class DisguiseCameraCapture : MonoBehaviour
     [Tooltip("Uncheck to control camera parameters from within the game engine\n.Check to control camera parameters from disguise")]
     public bool ApplyCameraData = true;
 
+    [HideInInspector]
+    public bool _dbgOutsideTransformChange;
+
     // Start is called before the first frame update
     public IEnumerator Start()
     {
@@ -622,78 +632,89 @@ public class DisguiseCameraCapture : MonoBehaviour
         // set tracking
         m_newFrameData = DisguiseRenderStream.newFrameData && m_frameSender != null && m_frameSender.GetCameraData(ref m_cameraData);
 
-        if (!ApplyCameraData) return;
-
-        float cameraAspect = m_camera.aspect;
-        Vector2 lensShift = new Vector2(0.0f, 0.0f);
-        if (m_newFrameData)
+        if (ApplyCameraData)
         {
-            cameraAspect = m_cameraData.sensorX / m_cameraData.sensorY;
-            if (m_cameraData.cameraHandle != 0)  // If no camera, only set aspect
+            float cameraAspect = m_camera.aspect;
+            Vector2 lensShift = new Vector2(0.0f, 0.0f);
+            if (m_newFrameData)
             {
-                transform.SetLocalPositionAndRotation(
-                    new Vector3(m_cameraData.x, m_cameraData.y, m_cameraData.z), 
-                    Quaternion.Euler(-m_cameraData.rx, m_cameraData.ry, -m_cameraData.rz)
-                );
-                m_camera.nearClipPlane = m_cameraData.nearZ;
-                m_camera.farClipPlane  = m_cameraData.farZ;
-
-                if (m_cameraData.orthoWidth > 0.0f)  // Use an orthographic camera
-                {  
-                    m_camera.orthographic = true;
-                    m_camera.orthographicSize = 0.5f * m_cameraData.orthoWidth / cameraAspect;
+                cameraAspect = m_cameraData.sensorX / m_cameraData.sensorY;
+                if (m_cameraData.cameraHandle != 0)  // If no camera, only set aspect
+                {
                     transform.SetLocalPositionAndRotation(
-                        new Vector3(m_cameraData.x, m_cameraData.y, m_cameraData.z), 
+                        new Vector3(m_cameraData.x, m_cameraData.y, m_cameraData.z),
                         Quaternion.Euler(-m_cameraData.rx, m_cameraData.ry, -m_cameraData.rz)
                     );
+                    m_camera.nearClipPlane = m_cameraData.nearZ;
+                    m_camera.farClipPlane = m_cameraData.farZ;
+
+                    if (m_cameraData.orthoWidth > 0.0f)  // Use an orthographic camera
+                    {
+                        m_camera.orthographic = true;
+                        m_camera.orthographicSize = 0.5f * m_cameraData.orthoWidth / cameraAspect;
+                        transform.SetLocalPositionAndRotation(
+                            new Vector3(m_cameraData.x, m_cameraData.y, m_cameraData.z),
+                            Quaternion.Euler(-m_cameraData.rx, m_cameraData.ry, -m_cameraData.rz)
+                        );
+                    }
+                    else  // Perspective projection, use camera lens properties
+                    {
+                        m_camera.usePhysicalProperties = true;
+                        m_camera.sensorSize = new Vector2(m_cameraData.sensorX, m_cameraData.sensorY);
+                        m_camera.focalLength = m_cameraData.focalLength;
+                        lensShift = new Vector2(-m_cameraData.cx, m_cameraData.cy);
+                    }
                 }
-                else  // Perspective projection, use camera lens properties
+            }
+            else if (m_frameSender != null)
+            {
+                // By default aspect is resolution aspect. We need to undo the effect of the subregion on this to get the whole image aspect.
+                cameraAspect = m_camera.aspect * (m_frameSender.subRegion.height / m_frameSender.subRegion.width);
+            }
+
+            // Clip to correct subregion and calculate projection matrix
+            if (m_frameSender != null)
+            {
+                Rect subRegion = m_frameSender.subRegion;
+
+                float imageHeight, imageWidth;
+                if (m_camera.orthographic)
                 {
-                    m_camera.usePhysicalProperties = true;
-                    m_camera.sensorSize = new Vector2(m_cameraData.sensorX, m_cameraData.sensorY);
-                    m_camera.focalLength = m_cameraData.focalLength;
-                    lensShift = new Vector2(-m_cameraData.cx, m_cameraData.cy);
+                    imageHeight = 2.0f * m_camera.orthographicSize;
+                    imageWidth = cameraAspect * imageHeight;
                 }
+                else
+                {
+                    float fovV = m_camera.fieldOfView * Mathf.Deg2Rad;
+                    float fovH = Camera.VerticalToHorizontalFieldOfView(m_camera.fieldOfView, cameraAspect) * Mathf.Deg2Rad;
+                    imageWidth = 2.0f * (float)Math.Tan(0.5f * fovH);
+                    imageHeight = 2.0f * (float)Math.Tan(0.5f * fovV);
+                }
+
+                float l = (-0.5f + subRegion.xMin) * imageWidth;
+                float r = (-0.5f + subRegion.xMax) * imageWidth;
+                float t = (-0.5f + 1.0f - subRegion.yMin) * imageHeight;
+                float b = (-0.5f + 1.0f - subRegion.yMax) * imageHeight;
+
+                Matrix4x4 projectionMatrix;
+                if (m_camera.orthographic)
+                    projectionMatrix = Matrix4x4.Ortho(l, r, b, t, m_camera.nearClipPlane, m_camera.farClipPlane);
+                else
+                    projectionMatrix = PerspectiveOffCenter(l * m_camera.nearClipPlane, r * m_camera.nearClipPlane, b * m_camera.nearClipPlane, t * m_camera.nearClipPlane, m_camera.nearClipPlane, m_camera.farClipPlane);
+
+                Matrix4x4 clippingTransform = Matrix4x4.Translate(new Vector3(-lensShift.x / subRegion.width, lensShift.y / subRegion.height, 0.0f));
+                m_camera.projectionMatrix = clippingTransform * projectionMatrix;
             }
         }
-        else if (m_frameSender != null)
+
+        if ((Time.frameCount % 100) == 0)
         {
-            // By default aspect is resolution aspect. We need to undo the effect of the subregion on this to get the whole image aspect.
-            cameraAspect = m_camera.aspect * (m_frameSender.subRegion.height / m_frameSender.subRegion.width);
-        }
-
-        // Clip to correct subregion and calculate projection matrix
-        if (m_frameSender != null)
-        {
-            Rect subRegion = m_frameSender.subRegion;
-            
-            float imageHeight, imageWidth;
-            if (m_camera.orthographic)
+            // Debug output every now and then
+            if (m_newFrameData || _dbgOutsideTransformChange)
             {
-                imageHeight = 2.0f * m_camera.orthographicSize;
-                imageWidth = cameraAspect * imageHeight;
+                Debug.Log($"dCameraCapture({gameObject.name}): newFrameData={m_newFrameData}, _dbgOutsideTransform={_dbgOutsideTransformChange}, pos={transform.localPosition}, rot={transform.localEulerAngles}");
+                _dbgOutsideTransformChange = false;
             }
-            else
-            {
-                float fovV = m_camera.fieldOfView * Mathf.Deg2Rad;
-                float fovH = Camera.VerticalToHorizontalFieldOfView(m_camera.fieldOfView, cameraAspect) * Mathf.Deg2Rad;
-                imageWidth = 2.0f * (float)Math.Tan(0.5f * fovH);
-                imageHeight = 2.0f * (float)Math.Tan(0.5f * fovV);
-            }
-
-            float l = (-0.5f + subRegion.xMin) * imageWidth;
-            float r = (-0.5f + subRegion.xMax) * imageWidth;
-            float t = (-0.5f + 1.0f - subRegion.yMin) * imageHeight;
-            float b = (-0.5f + 1.0f - subRegion.yMax) * imageHeight;
-
-            Matrix4x4 projectionMatrix;
-            if (m_camera.orthographic)
-                projectionMatrix = Matrix4x4.Ortho(l, r, b, t, m_camera.nearClipPlane, m_camera.farClipPlane);
-            else
-                projectionMatrix = PerspectiveOffCenter(l * m_camera.nearClipPlane, r * m_camera.nearClipPlane, b * m_camera.nearClipPlane, t * m_camera.nearClipPlane, m_camera.nearClipPlane, m_camera.farClipPlane);
-
-            Matrix4x4 clippingTransform = Matrix4x4.Translate(new Vector3(-lensShift.x / subRegion.width, lensShift.y / subRegion.height, 0.0f));
-            m_camera.projectionMatrix = clippingTransform * projectionMatrix;
         }
     }
 
